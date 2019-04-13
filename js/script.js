572e683936eb8a986a94a1b6ecfeb6d04214ce34
Romchik38 <nice.day.for.js@gmail.com>
2019-04-14 00:35:27 +0300

    День 1 - Показывается меню, Ночной режим, Кнопка Загрузить

---
+++ js/script.js
@@ -0,0 +1,85 @@
+'use strict';
+
+const switcher = document.querySelector('#cbx'),   //Кнопка ночного режима
+    more = document.querySelector('.more'),       //Кнопка загрузить еще
+    modal = document.querySelector('.modal'),
+    videos = document.querySelectorAll('.videos__item');  //Видеоролики
+
+let player;     //Плеер который будет подтягиваться с ютуба
+
+//Открытие и закрытие меню
+function bindSlideToggle(trigger, boxBody, content, openClass) {
+    let button = {
+        element: document.querySelector(trigger),
+        active: false,
+    };
+    const box = document.querySelector(boxBody),
+        boxContent = document.querySelector(content);
+    button.element.addEventListener('click', () => {
+        if (button.active === false) {      //Если меню закрыто
+            button.active = true;
+            box.style.height = boxContent.clientHeight + 'px';   //Меняется высота меню с 0 на высоту контента внутри него
+            box.classList.add(openClass)    //Активные класс для меню
+            console.log(box);
+        } else {
+            button.active = false;
+            box.style.height = 0 + 'px';   //Меняется высота меню с 0 на высоту контента внутри него
+            box.classList.remove(openClass);    // удаление Активные класс для меню
+        }
+    })
+}
+
+bindSlideToggle('.hamburger', '[data-slide="nav"]', '.header__menu', 'slide-active');
+
+//Переключение ночного режима
+function switchMode() {
+    if (night === false) nightColor('#fff', true, 'add', 'logo/youtube_night.svg');
+    else nightColor('#000', false, 'remove', 'logo/youtube.svg');
+}
+
+let night = false;
+
+switcher.addEventListener('change', switchMode);
+
+const nightColor = (color, background, cl, logo) => {
+    night = background; //фон
+    document.querySelectorAll('.hamburger > line').forEach(item => item.style.stroke = color);  //цвет полосок меню
+    document.body.classList[cl]('night');   // удаление или добавление класса
+    document.querySelectorAll('.videos__item-descr').forEach(item => item.style.color = color);  //цвет надписей
+    document.querySelectorAll('.videos__item-views').forEach(item => item.style.color = color);  //видимость просмотров
+    document.querySelector('.header__item-descr').style.color = color; // цвет надписи Night mode
+    document.querySelector('.logo > img').src = logo;  // цвет логотипа
+}
+//Подгрузка видео 
+const data = [
+    ['img/thumb_3.webp', 'img/thumb_4.webp', 'img/thumb_5.webp'],
+    ['#3 Верстка на flexbox CSS | Блок преимущества и галерея | Марафон верстки | Артем Исламов',
+        '#2 Установка spikmi и работа с ветками на Github | Марафон вёрстки Урок 2',
+        '#1 Верстка реального заказа landing Page | Марафон вёрстки | Артём Исламов'],
+    ['3,6 тыс. просмотров', '4,2 тыс. просмотров', '28 тыс. просмотров'],
+    ['X9SmcY3lM-U', '7BvHoh0BrMw', 'mC8JW_aG2EM']
+];
+more.addEventListener('click', () => {
+    const videosWrapper = document.querySelector('.videos__wrapper');
+    more.remove();
+    for (let i = 0; i < data[0].length; i++) {
+        let card = document.createElement('a');
+        card.classList.add('videos__item', 'videos__item-active'); // videos__item-active - opacity = 0 и смещение на 50;
+        card.setAttribute('data-url', data[3][i]);  //установка атрибута  
+        card.innerHTML = `
+        <img src="${data[0][i]}" alt="thumb">
+        <div class="videos__item-descr">
+            ${data[1][i]}
+        </div>
+        <div class="videos__item-views">
+            ${data[2][i]}
+        </div>
+        `;
+        videosWrapper.appendChild(card);
+        //Это нужно для того, чтобы картинки появились плавно со сдвигом
+        setTimeout(() => {
+            card.classList.remove('videos__item-active');   //после удаления этого класса opacity = 1 ;
+        }, 10);
+
+    }
+})
